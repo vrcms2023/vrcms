fe0ea7d9f6e3d40b6f6e28391c7b1b490b64ca8d
Ramakrishna <jetti@Ramakrishna.local>
2024-06-06 20:02:11 +0530

    changes

--- frontend/src/Frontend_Admin/Components/BriefIntro/SimpleTitleDescComponent.js
+++ frontend/src/Frontend_Admin/Components/BriefIntro/SimpleTitleDescComponent.js
@@ -2,11 +2,8 @@ import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 // Components
-import Button from "../../../Common/Button";
 import Title from "../../../Common/Title";
-import EditAdminPopupHeader from "../EditAdminPopupHeader";
-import { axiosServiceApi } from "../../../util/axiosUtil";
-import { getCookie } from "../../../util/cookieUtil";
+import { axiosClientServiceApi } from "../../../util/axiosUtil";
 import { Link } from "react-router-dom";
 
 export const SimpleTitleDescComponent = ({
@@ -18,7 +15,7 @@ export const SimpleTitleDescComponent = ({
   useEffect(() => {
     const getintroValues = async () => {
       try {
-        let response = await axiosServiceApi.get(formgetURL);
+        let response = await axiosClientServiceApi.get(formgetURL);
         setFormValues(response.data.intro);
       } catch (error) {
         console.log("Unable to get the intro");
@@ -33,7 +30,9 @@ export const SimpleTitleDescComponent = ({
     <>
       <Title title={formvalues?.intro_title} cssClass="fs-5 fw-medium mb-2" />
       <p>{formvalues?.intro_desc}</p>
-      <Link to={formvalues?.intro_morelink} className="btn btn-outline mt-3">More</Link>
+      <Link to={formvalues?.intro_morelink} className="btn btn-outline mt-3">
+        More
+      </Link>
     </>
   );
 };
