6a3dedaef85f233a0b3d3cc852e1d44dd39bf42d
Ramakrishna <ramajsworks@gmail.com>
2024-10-02 22:38:28 +0530

    edit icon changes

--- frontend/src/Common/AdminEditIcon.jsx
+++ frontend/src/Common/AdminEditIcon.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./AdminEditIcon.css";
 
-const EditIcon = ({ 
+const EditIcon = ({
   editHandler,
   icon = "fa-pencil",
   iconCss = "text-warning cursor-pointer fs-3",
-  cssClasses = "position-absolute "
- }) => {
+  cssClasses = "position-absolute ",
+}) => {
   return (
     <span className={`${cssClasses} editIcon`}>
       <i
         className={`fa ${icon} ${iconCss}`}
         aria-hidden="true"
-        onClick={() => editHandler("carousel", true)}
+        onClick={editHandler}
       ></i>
     </span>
   );
