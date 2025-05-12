100df9aa124000036847de6f4c8fc0819e3cd148
Ramakrishna <ramajsworks@gmail.com>
2025-05-12 10:07:19 +0530

    update changes

--- frontend/src/Frontend_Views/Components/RichTextEditor.jsx
+++ frontend/src/Frontend_Views/Components/RichTextEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ReactQuill from "react-quill-new";
 import "react-quill-new/dist/quill.snow.css";
 
